dcdf7074b3d40e5dd31ef3a1ab29c8dca55068e7
Denis <hallo-boy@mail.ru>
2022-08-29 09:33:37 +0300

    добавлены комментарии

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+//создаём обработчик клика на ластик
 document.querySelector("#eraser").addEventListener("click", () => {
+  //убираем весь контент по нажатию
   document.querySelector("#groceryItems").textContent = "";
 });
-
+//создаём обработчик добавления заметки по нажатию Enter
 document.querySelector("#userInput").addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     addItem();
   }
 });
-
+//функция создания заметки
 addItem = () => {
+  //создаём элемент который будем добавлять
   const item = document.createElement("h2");
+  //создаём значение добавляемого элемента
   item.textContent = "-" + document.querySelector(".userInput").value;
-
+  //добавляем событие по клику на добавленный элемент
   item.addEventListener("click", () => {
+    //если нажать на элемент он зачеркнется,если он уже зачеркнут,то оно убирается
     if (item.style.textDecoration != "line-through") {
       item.style.textDecoration = "line-through";
     } else {
       item.style.textDecoration = "none";
     }
   });
+  //добавляем созданный элемент в конец блока
   document.querySelector("#groceryItems").appendChild(item);
+  //очищаем поле ввода после добавления элемента
   document.querySelector(".userInput").value = "";
 };
